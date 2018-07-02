fb8363b9f6bc0daa79e19a62e3ee183fca6b2d1d
Jafar <j4ncisagent@gmail.com>
2018-07-02 15:22:04 +0700

    This is the folder for machine learning python study

---
+++ Maclearn/Checkversion.py
@@ -0,0 +1,20 @@
+# This file is used to check the version of each package that will be used
+
+# Python
+import sys
+print('Python: {}'.format(sys.version))
+# Scipy
+import scipy
+print("scipy: {}".format(scipy.__version__))
+# Numpy
+import numpy
+print("numpy: {}".format(numpy.__version__))
+# Pandas
+import pandas
+print("pandas: {}".format(pandas.__version__))
+# Matplotlib
+import matplotlib
+print("matplotlib: {}".format(matplotlib.__version__))
+# Sklearn
+import sklearn
+print("sklearn: {}".format(sklearn.__version__))
